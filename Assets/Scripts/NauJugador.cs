a76cc7387b356efd621054ef3d7b63c37b15fb1d
Arnau Rodríguez Pujals <a.rodriguez@ceroca.cat>
2024-09-23 20:24:26 +0200

    Límits pantalla

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class NauJugador : MonoBehaviour
 {
     private float _vel;
+
+    private Vector2 minPantalla, maxPantalla;
     
     // Start is called before the first frame update
     void Start()
     {
         _vel = 8f;
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
     }
 
     // Update is called once per frame
@@ -23,6 +27,10 @@ public class NauJugador : MonoBehaviour
         Vector2 novaPos = transform.position;// transform.position: pos actual de la nau.
         novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
         //Debug.Log(Time.deltaTime);
+
+        novaPos.x = Mathf.Clamp(novaPos.x, minPantalla.x, maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y, maxPantalla.y);
+        
         transform.position = novaPos;
     }
 }
